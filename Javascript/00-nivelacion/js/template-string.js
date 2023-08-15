5ab8cb688591dea096d6e148fa542448b3201b83
dev-jromero <n.bjhon@gmail.com>
2023-08-15 13:45:17 -0500

    add new files

--- Javascript/00-nivelacion/js/template-string.js
+++ Javascript/00-nivelacion/js/template-string.js
@@ -1,4 +1,5 @@
 const nombre = "Jhonatan";
 const edad = new Date().getFullYear()- 1991;
+const mensaje= `Bienvenido ${nombre}, tienes la edad de ${edad}`
 
-console.log("Bienvenido "+ nombre + " , tu edad es de "+ edad);
+console.log(mensaje);
